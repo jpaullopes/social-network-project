c06206cba1a45dbab40b56196ddd135b46cdcfc4
thalyssonDEV <contato.thalyssondel@gmail.com>
2025-01-26 12:25:51 -0300

    add: class PerfilAvancado

--- dist/models/PerfilAvancado.js
+++ dist/models/PerfilAvancado.js
@@ -3,5 +3,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.PerfilAvancado = void 0;
 const Perfil_1 = require("../models/Perfil");
 class PerfilAvancado extends Perfil_1.Perfil {
+    constructor(nome, email, senha) {
+        super(nome, email, senha);
+    }
+    // Método para habilitar o perfil de outro usuário
+    habilitarPerfil(perfil) {
+        // Verifica se o usuário é uma instância de PerfilAvancado, impedindo a modificação do status
+        if (perfil instanceof PerfilAvancado) {
+            return false; // Retorna false se o perfil for avançado
+        }
+        else {
+            perfil.setStatus("ativo"); // Altera o status do perfil para ativo
+            return true; // Retorna true se o perfil for normal
+        }
+    }
+    // Método para desabilitar o perfil de outro usuário
+    desabilitarPerfil(perfil) {
+        // Verifica se o usuário é uma instância de PerfilAvancado, impedindo a modificação do status
+        if (perfil instanceof PerfilAvancado) {
+            return false; // Retorna false se o perfil for avançado
+        }
+        else {
+            perfil.setStatus("inativo"); // Altera o status do perfil para inativo
+            return true; // Retorna true se o perfil for normal
+        }
+    }
 }
 exports.PerfilAvancado = PerfilAvancado;
--- src/models/PerfilAvancado.ts
+++ src/models/PerfilAvancado.ts
@@ -1,5 +1,40 @@
 import { Perfil } from "../models/Perfil";
 
 export class PerfilAvancado extends Perfil {
-    // EM DESENVOLVIMENTO
-}
+
+    constructor(nome: string, email: string, senha: string){
+        super(nome, email, senha);
+    }
+
+    // Método para habilitar o perfil de outro usuário
+    public habilitarPerfil(perfil: Perfil): boolean {
+
+        // Verifica se o usuário é uma instância de PerfilAvancado, impedindo a modificação do status
+        if (perfil instanceof PerfilAvancado) {
+
+            return false // Retorna false se o perfil for avançado
+
+        } else {
+            perfil.setStatus("ativo"); // Altera o status do perfil para ativo
+
+            return true // Retorna true se o perfil for normal
+        }
+    }
+
+
+    // Método para desabilitar o perfil de outro usuário
+    public desabilitarPerfil(perfil: Perfil): boolean {
+
+        // Verifica se o usuário é uma instância de PerfilAvancado, impedindo a modificação do status
+        if (perfil instanceof PerfilAvancado) {
+
+            return false // Retorna false se o perfil for avançado
+
+        } else {
+            perfil.setStatus("inativo"); // Altera o status do perfil para inativo
+            
+            return true // Retorna true se o perfil for normal
+        }
+
+    }
+} 
